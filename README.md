6d70bb4a0fccd3651a889f73a43b6e327641b859
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2022-10-12 08:33:51 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Nokia-OZO-Audio-Capture-Redmi-K40-Magisk-Module
+# Nokia OZO Audio Capture Redmi K40 Magisk Module
+
+## DISCLAIMER
+- Nokia OZO Audio Capture blobs are owned by Nokia™.
+- The MIT license specified here is for the Magisk Module, not for Nokia OZO Audio Capture blobs.
+
+## Descriptions
+- Audio quality enhancements for audio/video recordings ported from Xiaomi Redmi K40 (alioth) and integrated as a Magisk Module for all supported and rooted devices with Magisk
+- Pre process type soundfx
+- Any audio/video recorder app must have android.permission.MODIFY_AUDIO_SETTINGS to be able to connect to this soundfx
+
+## Sources
+- https://dumps.tadiphone.dev/dumps/redmi/alioth qssi-user-12-SKQ1.211006.001-V13.0.3.0.SKHEUXM-release-keys
+- ozoplaysdk.license: https://github.com/something6969/vendor_nokia_TheThing/tree/e5ae540fba9441978a03af9577a89ae50b882b6b
+- ozosdk.license: https://github.com/something6969/vendor_nokia_TheThing/tree/e5ae540fba9441978a03af9577a89ae50b882b6b
+
+## Screenshots
+- https://t.me/androidryukimodsdiscussions/113355
+
+## Requirements
+- Magisk installed
+
+## Installation Guide & Download Link
+- Reboot
+- Install this module via Magisk Manager or Recovery
+- Install AML Magisk Module https://zackptg5.com/android.php#aml only if using any other audio mod module
+- Reboot
+
+## Optionals
+- https://t.me/androidryukimodsdiscussions/60861
+- https://t.me/androidryukimodsdiscussions/26764
+
+## Troubleshootings
+- https://t.me/androidryukimodsdiscussions/29836
+
+## Support & Bug Report
+- https://t.me/androidryukimodsdiscussions/2618
+- If you don't do above, issues will be closed immediately
+
+## Tested on
+- Android 13 AOSP ROM
+
+## Credits and contributors
+- https://t.me/viperatmos
+- https://t.me/androidryukimodsdiscussions
+- You can contribute ideas about this Magisk Module here: https://t.me/androidappsportdevelopment
+
+## Thanks for Donations
+- This Magisk Module is always will be free but you can however show us that you are care by making a donations:
+- https://ko-fi.com/reiryuki
+- https://www.paypal.me/reiryuki
+- https://t.me/androidryukimodsdiscussions/2619
+
+
---
+++ post-fs-data.sh
@@ -0,0 +1,94 @@
+mount -o rw,remount /data
+MODPATH=${0%/*}
+AML=/data/adb/modules/aml
+ACDB=/data/adb/modules/acdb
+
+# debug
+exec 2>$MODPATH/debug-pfsd.log
+set -x
+
+# run
+FILE=$MODPATH/sepolicy.sh
+if [ -f $FILE ]; then
+  sh $FILE
+fi
+
+# context
+chcon -R u:object_r:vendor_file:s0 $MODPATH/system/vendor
+chcon -R u:object_r:vendor_configs_file:s0 $MODPATH/system/vendor/etc
+chcon -R u:object_r:vendor_configs_file:s0 $MODPATH/system/vendor/odm/etc
+# etc
+if [ -d /sbin/.magisk ]; then
+  MAGISKTMP=/sbin/.magisk
+else
+  MAGISKTMP=`find /dev -mindepth 2 -maxdepth 2 -type d -name .magisk`
+fi
+ETC=$MAGISKTMP/mirror/system/etc
+VETC=$MAGISKTMP/mirror/system/vendor/etc
+VOETC=$MAGISKTMP/mirror/system/vendor/odm/etc
+MODETC=$MODPATH/system/etc
+MODVETC=$MODPATH/system/vendor/etc
+MODVOETC=$MODPATH/system/vendor/odm/etc
+
+# conflict
+if [ -d $AML ] && [ ! -f $AML/disable ]\
+&& [ -d $ACDB ] && [ ! -f $ACDB/disable ]; then
+  touch $ACDB/disable
+fi
+
+# directory
+SKU=`ls $VETC/audio | grep sku_`
+if [ "$SKU" ]; then
+  for SKUS in $SKU; do
+    mkdir -p $MODVETC/audio/$SKUS
+  done
+fi
+PROP=`getprop ro.build.product`
+if [ -d $VETC/audio/"$PROP" ]; then
+  mkdir -p $MODVETC/audio/"$PROP"
+fi
+
+# audio files
+NAME="*audio*effects*.conf -o -name *audio*effects*.xml"
+rm -f `find $MODPATH/system -type f -name $NAME`
+A=`find $ETC -maxdepth 1 -type f -name $NAME`
+VA=`find $VETC /odm/etc /my_product/etc -maxdepth 1 -type f -name $NAME`
+VOA=`find $VOETC -maxdepth 1 -type f -name $NAME`
+VAA=`find $VETC/audio -maxdepth 1 -type f -name $NAME`
+VBA=`find $VETC/audio/"$PROP" -maxdepth 1 -type f -name $NAME`
+if [ "$A" ]; then
+  cp -f $A $MODETC
+fi
+if [ "$VA" ]; then
+  cp -f $VA $MODVETC
+fi
+if [ "$VOA" ]; then
+  cp -f $VOA $MODVOETC
+fi
+if [ "$VAA" ]; then
+  cp -f $VAA $MODVETC/audio
+fi
+if [ "$VBA" ]; then
+  cp -f $VBA $MODVETC/audio/"$PROP"
+fi
+if [ "$SKU" ]; then
+  for SKUS in $SKU; do
+    VSA=`find $VETC/audio/$SKUS -maxdepth 1 -type f -name $NAME`
+    if [ "$VSA" ]; then
+      cp -f $VSA $MODVETC/audio/$SKUS
+    fi
+  done
+fi
+rm -f `find $MODPATH/system -type f -name *audio*effects*spatializer*.xml`
+
+# run
+sh $MODPATH/.aml.sh
+
+# cleaning
+FILE=$MODPATH/cleaner.sh
+if [ -f $FILE ]; then
+  sh $FILE
+  rm -f $FILE
+fi
+
+
